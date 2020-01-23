c64e02c5a622abc2a525e9c325ea4aef234f2bd7
Woz4tetra <woz4tetra@gmail.com>
2020-01-23 11:05:19 -0500

    bno.enterNormalMode causing issues

--- lib/Rover6/rover6_bno.h
+++ lib/Rover6/rover6_bno.h
@@ -39,7 +39,7 @@ void setup_BNO055()
         is_bno_setup = true;
         println_info("BNO055 initialized.");
     }
-    bno.enterNormalMode();
+    // bno.enterNormalMode();
 }
 
 bool read_BNO055()
