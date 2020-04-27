667ea6aec10c542de5bb9fa03a694c11054454c6
Woz4tetra <woz4tetra@gmail.com>
2020-04-26 23:00:56 -0400

    fixing up launch files

--- rover6_ros/make.sh
+++ rover6_ros/make.sh
@@ -6,5 +6,5 @@ ${BASE_DIR}/copy.sh
 cd $HOME/ros_ws
 catkin_make
 # catkin_make  -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.7m -DPYTHON_LIBRARY=/usr/lib/arm-linux-gnueabihf/libpython3.7m.so.1.0
-catkin_make -DCATKIN_BLACKLIST_PACKAGES="rtabmap_ros"
+# catkin_make -DCATKIN_BLACKLIST_PACKAGES="rtabmap_ros"
 cd -
